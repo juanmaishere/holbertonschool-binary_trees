f9e6da0b3ebe695b2956cdf2082dfc32adb549a5
Juanmaishere <rsjuanmanuel@hotmail.com>
2023-09-02 19:37:52 +0000

    feat tree

---
+++ 0-binary_tree_node.c
@@ -0,0 +1,20 @@
+#include "binary_trees.h"
+binary_tree_t
+*binary_tree_node(binary_tree_t *parent, int value)
+{
+    binary_tree_t *newnode;
+
+    newnode = malloc(sizeof(binary_tree_t));
+
+    if(!newnode)
+    {
+    return (NULL);
+    }
+
+    newnode->n = value;
+    newnode->parent = parent;
+    newnode->right = NULL;
+    newnode->left = NULL;
+
+return (newnode);
+}
