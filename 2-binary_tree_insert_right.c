7c6480e97a3d3cef102796b1723b82da0d627334
Juanmaishere <rsjuanmanuel@hotmail.com>
2023-09-02 20:04:25 +0000

    feat

---
+++ 2-binary_tree_insert_right.c
@@ -0,0 +1,30 @@
+#include "binary_trees.h"
+binary_tree_t
+*binary_tree_insert_left(binary_tree_t *parent, int value)
+{
+binary_tree_t *newnode;
+
+if(!parent)
+{
+    return (NULL);
+}
+newnode = malloc(sizeof(binary_tree_t));
+
+if(!newnode)
+{
+    return (NULL);
+}    
+newnode->n = value;
+newnode->parent = parent;
+newnode->left = NULL;
+newnode->right = NULL;
+   
+if (parent->right != NULL)
+{
+newnode->right = parent->right;
+parent->right->parent = newnode;
+}
+parent->right = newnode;
+
+return(newnode);
+}
