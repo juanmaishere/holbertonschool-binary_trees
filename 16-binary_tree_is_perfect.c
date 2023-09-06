e2cc9203f525bf088bf50823082f2ba467d8e027
JOTALGS <jose,pgilsuarez@gmail.com>
2023-09-05 19:42:26 -0700

    16 final?

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -4,18 +4,24 @@ int status = 1;
 int
 is_perfect(const binary_tree_t *tree)
 {
+	size_t l_height, r_height;
+
 	if (tree)
 	{
-		if (!((tree->right && tree->left) || (!(tree->right) && !(tree->left))))
+		l_height = binary_tree_height(tree->left);
+		r_height = binary_tree_height(tree->right);
+		if (!((tree->right && tree->left) || (!(tree->right) && !(tree->left))) || (l_height != r_height))
 			status = 0;
 		is_perfect(tree->right);
 		is_perfect(tree->left);
 	}
-	return (0);
+	return (status);
 }
 int
 binary_tree_is_perfect(const binary_tree_t *tree)
 {
+	if (!tree)
+		return (0);
 	is_perfect(tree);
 	if (status == 1)
 		return (1);
