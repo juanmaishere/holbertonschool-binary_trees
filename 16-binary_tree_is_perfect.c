091f0f6db168bc0f50d392b5ddebb87fca327b0e
JOTALGS <jose,pgilsuarez@gmail.com>
2023-09-05 19:07:39 -0700

    16-fixed

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,36 +1,24 @@
 #include "binary_trees.h"
+int status = 1;
 
+int
+is_perfect(const binary_tree_t *tree)
+{
+	if (tree)
+	{
+		if (!((tree->right && tree->left) || (!(tree->right) && !(tree->left))))
+			status = 0;
+		is_perfect(tree->right);
+		is_perfect(tree->left);
+	}
+	return (0);
+}
 int
 binary_tree_is_perfect(const binary_tree_t *tree)
 {
-	const binary_tree_t *node = tree;
-	unsigned int r_levels = 0;
-	unsigned int l_levels = 0;
-
-	if (!tree)
-		return (0);
-	while (node->left)
-	{
-		if (!(node->right))
-			return (0);
-		node = node->left;
-		l_levels++;
-	}
-	if (node->right)
-		return (0);
-	while (node->parent)
-		node = node->parent;
-	while (node->right)
-	{
-		if(!(node->left))
-			return (0);
-		node = node->right;
-		r_levels++;
-	}
-	if (node->left)
-		return (0);
-	if (l_levels == r_levels)
+	is_perfect(tree);
+	if (status == 1)
 		return (1);
 	else
-		return(0);
+		return (0);
 }
